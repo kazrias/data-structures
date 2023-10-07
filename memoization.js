a5e8c76337e706c6ea486b7051246c12f45999f7
Artyom Harutyunyan <ka6rias@gmail.com>
2023-10-07 16:59:57 +0400

    added additional memoization implementation for fibonnaci recursive function + comments for myself

--- memoization.js
+++ memoization.js
@@ -1,16 +1,30 @@
 function fibonacci(n) {
   return n <= 1 ? n : fibonacci(n - 2) + fibonacci(n - 1)
 }
-
+//memoization function for recursive function (Remembers only the very first argument with which the function was called)
 function memo(f) {
-  const reccursionMap = new Map();
+  const recursionMap = new Map();
   return function (n) {
-    if (!reccursionMap.has(n)) {
+    if (!recursionMap.has(n)) {
       let res = f(n);
-      reccursionMap.set(n, res)
+      recursionMap.set(n, res)
     }
-    return reccursionMap.get(n)
+    return recursionMap.get(n)
   }
 }
+const memoizFib = memo(fibonacci);
 
-const fibMemo = memo(fibonacci);
+//memoization for a recursive fibonacci function (remembers all argument with which the function was called)
+function memoFib() {
+  const map = new Map()
+  return function fibonacci(n) {
+    if (!map.has(n)) {
+      let res = n <= 1 ? n : fibonacci(n - 2) + fibonacci(n - 1)
+      map.set(n, res)
+    }
+    return map.get(n)
+  }
+}
+const fib = memoFib();
+
+
